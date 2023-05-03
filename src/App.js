5abb35753456485ed05a3dd7e2872c82aa67bf33
Azra Spahić-Haderer <azbogsunca@gmail.com>
2023-05-03 13:26:42 +0200

    Add first code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
-  return <div className="App"></div>;
+  const App = () => {
+    const [hex, setHex] = useState('#ffffff');
+    const generatedHex = () => {
+      const randomColor = '#' + Math.floor(Math.random());
+      setHex(randomColor);
+    };
+    return (
+      <div className="App">
+        <p>{hex}</p>
+        <button onClick={generatedHex}>Generate</button>
+      </div>
+    );
+  };
 }
